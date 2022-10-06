152435a5134f77a967c1635ae121335252206c43
Сергей <sergemartin@yandex.ru>
2022-10-06 21:15:04 +0300

    Решил Задачу 34

---
+++ task34/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача № 34
+Console.Clear();
+int co = 0;
+int[] mass = new int[10];
+
+for (int i = 0; i < mass.Length; i++)
+{
+    mass[i] = new Random().Next(100, 1000);
+}
+int Chet(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) co++;
+    }
+    return co;
+}
+
+
+
+Console.WriteLine("[" + string.Join(",", mass) + "]");
+Console.Write($"Количество четных чисел в массиве = {Chet(mass)}.");
