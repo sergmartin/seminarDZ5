42175aae1909d8a6649d9ae033efaae8488b216a
Сергей <sergemartin@yandex.ru>
2022-10-06 21:28:50 +0300

    Решил задачу 36

---
+++ task36/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача № 36
+Console.Clear();
+int summ = 0;
+int[] mass = new int[10];
+for (int i = 0; i < mass.Length; i++)
+{
+    mass[i] = new Random().Next(-10, 10);
+}
+for (int i = 0; i < mass.Length; i++)
+{
+    if (i % 2 != 0)
+    {
+        summ+=mass[i];
+    }
+}
+
+Console.WriteLine("[" + string.Join(",", mass) + "]");
+Console.Write($"Сумма чисел на нечетных позициях массива равна: {summ}");
